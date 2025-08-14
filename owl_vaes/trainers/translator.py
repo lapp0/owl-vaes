357c5d29ab1938b1ccb0c0d77e6d1c4cf933f12a
Andrew Lapp <andrew@github.rew.la>
2025-08-14 15:05:41 -0400

    log rgb shape

--- owl_vaes/trainers/translator.py
+++ owl_vaes/trainers/translator.py
@@ -399,7 +399,7 @@ class OnlineLatentBridge(nn.Module):
         loss_reduction: str = "mean",
         loss_only: bool = True,
     ):
-        assert x_rgb.ndim == 5 and x_rgb.shape[2] == 3, x_rgb.shape
+        assert x_rgb.ndim == 5 and x_rgb.shape[2] == 3, str(x_rgb.shape)
 
         # preprocess + encode
         x_wan, x_owl = self.pp(x_rgb)
