136dbde293a0d869e3396d3557c5202b97237aa0
Andrew Lapp <andrew@github.rew.la>
2025-08-14 14:15:48 -0400

    fix

--- owl_vaes/trainers/translator.py
+++ owl_vaes/trainers/translator.py
@@ -440,13 +440,9 @@ class OnlineLatentBridge(nn.Module):
 
 
 class OnlineLatentTrainer(BaseTrainer):
-    def __init__(self, train_cfg, logging_cfg, model_cfg,
-                 global_rank=0, local_rank=0, world_size=1, **kwargs):
-        super().__init__(train_cfg, logging_cfg, model_cfg,
-                         global_rank=global_rank, local_rank=local_rank, world_size=world_size, **kwargs)
-        torch.cuda.set_device(self.local_rank)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.total_step_counter = 0
-
         self.opt = None
         self.scheduler = None
 
